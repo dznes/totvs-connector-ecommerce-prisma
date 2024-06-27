a225d3c7d7b2d5ce65630e125f4386eccbd9cd1e
dzns <davidkim1131@gmail.com>
2024-06-27 10:43:48 -0300

    refactored sku-detail-backup looping with while and pager validation to hasNext atribute from response, and add to database before fetching more items

--- src/http/controllers/sku/sku-details-backup.ts
+++ src/http/controllers/sku/sku-details-backup.ts
@@ -10,61 +10,50 @@ export async function skuDetailsBackup(
   try {
     const token = await fetchToken()
     const pageSize = 500
+    const daysStartFromToday = 100
+    const daysEndFromToday = 70
     let page = 1
-    let daysStartFromToday = 100
-    let daysEndFromToday = 100
-    let skuDetails: SkuDetail[] = []
 
-    // Fetch the initial page to get the total number of pages.
-    const initialData = await getProductInfos({
-      token: token.access_token,
-      page,
-      pageSize,
-      daysStartFromToday,
-      daysEndFromToday,
-    })
-    skuDetails = skuDetails.concat(initialData.items)
-    const totalPages = initialData.totalPages
-    console.log(totalPages)
+    const upsertSkuUseCase = makeUpsertSkuUseCase()
+    let hasNextValidator = true
 
-    // Fetch remaining pages
-    for (page = 2; page <= totalPages; page++) {
-      const data = await getProductInfos({
+    while (hasNextValidator) {
+      const { items, hasNext } = await getProductInfos({
         token: token.access_token,
         page,
         pageSize,
         daysStartFromToday,
         daysEndFromToday,
       })
-      const pageSkus = data.items
-      skuDetails = skuDetails.concat(pageSkus) // Accumulate items from each page
-      console.log(`${skuDetails.length} items processados.`)
-      // console.log(data)
+      // Upsert (create or update) sku info in Database before fetching more items
+      items.map((skuDetail: SkuDetail) => {
+        upsertSkuUseCase.execute({
+          code: skuDetail.productCode.toString(), // productCode is number in the API response
+          status: 200,
+          title: skuDetail.productName,
+          ean: skuDetail.productSku ?? '',
+          ncm: skuDetail.ncm,
+          mpn: skuDetail.productSku ?? '',
+          reference_id: skuDetail.referenceId.toString(),
+          reference_name: skuDetail.referenceName,
+          integration_code: 'TOTVS',
+          colorCode: skuDetail.colorCode,
+          colorTitle: skuDetail.colorName,
+          sizeCode: skuDetail.size,
+        })
+      })
+
+      // Break the loop if there are no more pages to fetch
+      if (!hasNext) {
+        hasNextValidator = false
+        break
+      }
+
+      page++
     }
 
-    const upsertSkuUseCase = makeUpsertSkuUseCase()
-
-    // Upsert (create or update) product info in Database
-    skuDetails.map((skuDetail) => {
-      upsertSkuUseCase.execute({
-        code: skuDetail.productCode.toString(),
-        status: 200,
-        title: skuDetail.productName,
-        ean: skuDetail.productSku ?? '',
-        ncm: skuDetail.ncm,
-        mpn: skuDetail.productSku ?? '',
-        reference_id: skuDetail.referenceId.toString(),
-        reference_name: skuDetail.referenceName,
-        integration_code: 'TOTVS',
-        colorCode: skuDetail.colorCode,
-        colorTitle: skuDetail.colorName,
-        sizeCode: skuDetail.size,
-      })
-    })
-    const skusCount = skuDetails.length
-
     // Return the complete list of items in the API response
-    return reply.status(200).send(JSON.stringify({ skusCount, totalPages }))
+    return reply.status(200).send(JSON.stringify({ pages: page }))
   } catch (err) {
     console.log(err)
     // It's better to return an HTTP error response
